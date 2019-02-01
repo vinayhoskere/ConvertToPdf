c2e97c8192f4d1373c34f89a0f63d8ccafc0089f
vhoskere <vhoskere@epharmasolutions.com>
2019-02-01 15:35:21 -0500

    Convert Image files to pdf

---
+++ ConvertToPdfs/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ConvertToPdfs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectedFiles_Click(object sender, RoutedEventArgs e)
+        {
+            lblSummary.Content = string.Empty;
+            var fileNames = new List<string>();
+
+            using (var dialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                dialog.Multiselect = true;
+
+                var result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    foreach (var fileName in dialog.FileNames)
+                    {
+                        fileNames.Add(fileName);
+                    }
+                }
+            }
+
+            listView.ItemsSource = fileNames;
+        }
+
+        private void btnConvertedFilePath_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new CommonOpenFileDialog())
+            {
+                dialog.IsFolderPicker = true;
+
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    txtConvertedFilePath.Text = dialog.FileName;
+                }
+            }
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateInput())
+            {
+                var fileName = txtFileName.Text;
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    fileName = "ConvertedFile";
+                }
+
+                fileName = txtConvertedFilePath.Text + "\\" + fileName + ".pdf";
+
+                var msg = PDFWrapper.CreatePDF(listView.Items, fileName);
+
+                lblSummary.Content = msg;
+
+                if (msg == "success")
+                {
+                    lblSummary.Content = "Files converted successfully.";
+                    lblSummary.Foreground = new SolidColorBrush(Colors.Green);
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (listView.Items != null)
+            {
+                listView.ItemsSource = null;
+            }
+
+            txtConvertedFilePath.Text = string.Empty;
+            txtFileName.Text = string.Empty;
+        }
+
+        private bool ValidateInput()
+        {
+            var isSuccess = true;
+            var errorMsg = new StringBuilder();
+
+            if (listView.Items.Count <= 0)
+            {
+                errorMsg.AppendLine("Please enter files to convert to pdf.");
+                isSuccess = false;
+            }
+
+            if (string.IsNullOrEmpty(txtConvertedFilePath.Text))
+            {
+                errorMsg.AppendLine("Please enter the location");
+                isSuccess = false;
+            }
+
+            lblSummary.Content = errorMsg.ToString();
+            lblSummary.Foreground = new SolidColorBrush(Colors.Red);
+
+            return isSuccess;
+        }
+    }
+}
---
+++ ConvertToPdfs/PDFWrapper.cs
@@ -0,0 +1,65 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Windows.Controls;
+using Image = iTextSharp.text.Image;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace ConvertToPdfs
+{
+    public class PDFWrapper
+    {
+        public static string CreatePDF(ItemCollection files, string fileName)
+        {
+            try
+            {
+                var document = new Document(PageSize.LETTER);
+
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                PdfWriter.GetInstance(document, new FileStream(fileName, FileMode.Create));
+
+                // step 3: we open the document
+                document.Open();
+
+                foreach (var file in files)
+                {
+                    var pic = Image.GetInstance(file.ToString());
+
+                    if (pic.Height > pic.Width)
+                    {
+                        //Maximum height is 800 pixels.
+                        float percentage = 0.0f;
+                        percentage = 700 / pic.Height;
+                        pic.ScalePercent(percentage * 100);
+                    }
+                    else
+                    {
+                        //Maximum width is 600 pixels.
+                        float percentage = 0.0f;
+                        percentage = 540 / pic.Width;
+                        pic.ScalePercent(percentage * 100);
+                    }
+
+                    pic.Border = Rectangle.BOX;
+                    pic.BorderColor = BaseColor.BLACK;
+                    pic.BorderWidth = 3f;
+                    document.Add(pic);
+                    document.NewPage();
+                }
+
+                document.Close();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return "Success";
+        }
+    }
+}
